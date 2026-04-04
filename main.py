7e15babc38002c6a4bdaa2f6e35b740c0dfa8d13
Casimir Rönnlöf <casimirr04@gmail.com>
2026-04-04 22:25:30 +0300

    manual coding :( but at least now working trailing zero calculator

--- main.py
+++ main.py
@@ -3,5 +3,27 @@
 
 import mmh3
 
-h = mmh3.hash64("hello world", signed=False)[0]
-#print(h)
+h = mmh3.hash64("Casimirasdasdasdadasd", signed=False)[0]
+print("The hash is: ", h)
+print("The binary of the hash is: ", bin(h))
+
+def num_of_zeros(v: int):
+    # simple example of bit manipulation in python
+    # nb = v.bit_length()
+    # binary = []
+    # for bit in range(nb-1, -1, -1):
+    #     binary.append((v >> bit) & 1);
+    # return binary
+    nb = v.bit_length()
+    num = 0
+    while(num < nb and (v >> num) & 1 == 0):
+        num += 1
+    
+    return num
+
+print(num_of_zeros(h))
+
+def add(v):
+    # extract the low P bits to get the register index
+    # how to find the position of the first 1 bit
+    pass
