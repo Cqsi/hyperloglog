ad73186a59bbf4c8eaa1624d1ff703b7a5c467ef
Casimir Rönnlöf <casimirr04@gmail.com>
2026-04-04 23:31:02 +0300

    first function done

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Your first step is implementing add — the function that takes an object, hashes it, and updates the right register.
 # To do that you need to figure out two things from the hash integer: how to extract the low P bits to get the register index, and how to find the position of the first 1 bit in the remaining upper bits. Both are just integer bit operations. Get those two things working and printing correctly before touching anything else.
 
+ # simple example of bit manipulation in python
+    # nb = v.bit_length()
+    # binary = []
+    # for bit in range(nb-1, -1, -1):
+    #     binary.append((v >> bit) & 1);
+    # return binary
+
 # P = number of registers
 # larger P = larger memory, more accuracy
 
@@ -14,29 +21,26 @@ print("The binary of the hash is: ", bin(h))
 
 M = array('B', [0] * 2**b) # 2^b unsigned byte registers
 
+# 1. hash
+# 2. find register index
+# 3. compute trailing zeros
+# 4. update register
 def add(v: int):
-    # simple example of bit manipulation in python
-    # nb = v.bit_length()
-    # binary = []
-    # for bit in range(nb-1, -1, -1):
-    #     binary.append((v >> bit) & 1);
-    # return binary
-    h = mmh3.hash64(v, signed=False)[0]
+    h = mmh3.hash64(str(v), signed=False)[0]
+    print("hash: ", h)
     
-    beg = (1 << b) - 1 # j in the paper
-    end = h >> b
+    mask = (1 << b) - 1
+    j = h & mask
+    print("j is ", j)
+    w = h >> b
+    print("w is ", w)
    
     num = 0
-    ne = end.bit_length()
-    while(num < ne and (end >> num) & 1 == 0):
+    ne = w.bit_length()
+    while(num < ne and (w >> num) & 1 == 0):
         num += 1
     
-    M[beg] = max(M[beg], num)
+    M[j] = max(M[j], num)
     
 
-print(add("Casimir"))
-
-def add(v):
-    # extract the low P bits to get the register index
-    # how to find the position of the first 1 bit
-    pass
+print(add(1234232))
